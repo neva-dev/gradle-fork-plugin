86c40357607bf331642c991a72c6e5756c33347f
Krystian Panek <krystian.panek@cognifide.com>
2018-05-23 08:14:47 +0200

    Properties task

---
+++ src/main/kotlin/com/neva/gradle/fork/PropertiesTask.kt
@@ -0,0 +1,32 @@
+package com.neva.gradle.fork
+
+import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.TaskAction
+
+open class PropertiesTask : DefaultTask() {
+
+  @Internal
+  private val fork = ForkTask.of(project)
+
+  init {
+    description = "Generates user specific 'gradle.properties' file basing on template and prompted values."
+
+    fork.inPlaceConfig(CONFIG_NAME, { copyTemplateFile(TEMPLATE_FILE) })
+  }
+
+  @TaskAction
+  fun properties() {
+    fork.config(CONFIG_NAME).evaluate()
+  }
+
+  companion object {
+
+    const val NAME = "properties"
+
+    const val CONFIG_NAME = "properties"
+
+    const val TEMPLATE_FILE = "$CONFIG_NAME/gradle.properties"
+
+  }
+
+}
