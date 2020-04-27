c1bcd66a11bef1121bb6ed9670782898899fa065
Krystian Panek <krystian.panek@cognifide.com>
2020-04-27 08:40:56 +0200

    Release fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
         id("org.jetbrains.kotlin.jvm") version "${extra["kotlin.version"]}"
         id("io.gitlab.arturbosch.detekt") version "${extra["detekt.version"]}"
         id("org.jetbrains.dokka") version "${extra["dokka.version"]}"
-        id("com.gradle.plugin-publish") version "0.10.1"
+        id("com.gradle.plugin-publish") version "0.11.0"
         id("com.jfrog.bintray") version "1.8.4"
         id("net.researchgate.release") version "2.6.0"
         id("com.github.breadmoirai.github-release") version "2.2.9"
