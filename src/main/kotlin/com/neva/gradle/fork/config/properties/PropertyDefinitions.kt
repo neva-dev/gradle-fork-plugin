1faec8d0dbfe67700d1b0cb10b1b7b8ae3a2a283
Krystian Panek <krystian.panek@cognifide.com>
2020-08-21 23:32:40 +0200

    Grouping callback

--- src/main/kotlin/com/neva/gradle/fork/config/properties/PropertyDefinitions.kt
+++ src/main/kotlin/com/neva/gradle/fork/config/properties/PropertyDefinitions.kt
@@ -2,31 +2,38 @@ package com.neva.gradle.fork.config.properties
 
 import com.neva.gradle.fork.ForkExtension
 import org.gradle.api.Action
-import org.gradle.internal.Actions
 
 class PropertyDefinitions(val fork: ForkExtension) {
 
-  private val definitions = mutableMapOf<String, PropertyDefinition>()
+  private var definitions = mutableMapOf<String, PropertyDefinition>()
 
   val all: List<PropertyDefinition>
     get() = definitions.values.toList()
 
   fun define(name: String, action: Action<in PropertyDefinition>): PropertyDefinition {
-    val definition = Actions.with(fork.project.objects.newInstance(PropertyDefinition::class.java, name), action)
-    definitions += name to definition
-    return definition
+    return fork.project.objects.newInstance(PropertyDefinition::class.java, name).apply {
+      action.execute(this)
+      definitions[name] = this
+    }
   }
 
   fun define(definitions: Map<String, PropertyDefinition.() -> Unit>): List<PropertyDefinition> {
-    return definitions.map { (name, options) -> define(name, Action { options(it) }) }
-  }
-
-  fun define(group: String, definitions: Map<String, PropertyDefinition.() -> Unit>): List<PropertyDefinition> {
-    return define(Action { it.group = group }, definitions)
+    return definitions.map { (name, options) -> define(name) { options(it) } }
   }
 
   fun define(commonDefinition: Action<in PropertyDefinition>, definitions: Map<String, PropertyDefinition.() -> Unit>): List<PropertyDefinition> {
     return define(definitions).apply { forEach { commonDefinition.execute(it) } }
+  }
+
+  fun define(group: String, definitions: Map<String, PropertyDefinition.() -> Unit>): List<PropertyDefinition> {
+    return define({ it.group = group }, definitions)
+  }
+
+  fun group(group: String, action: Action<in PropertyDefinitions>) {
+    val previousDefinitions = this.definitions
+    this.definitions = mutableMapOf()
+    action.execute(this)
+    this.definitions = (previousDefinitions + this.definitions.onEach { it.value.group = group }).toMutableMap()
   }
 
   fun get(name: String): PropertyDefinition? = definitions[name]
